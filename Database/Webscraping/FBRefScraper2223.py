5ef6d9c89aed957685827bf6d4731995f85d3ea5
Reece Jackson <Rjackson2000@icloud.com>
2023-07-03 21:11:37 +0100

    feat(FBRefScraper2223.py): finishing touches on scraping the stats from fbref

--- Database/Webscraping/FBRefScraper2223.py
+++ Database/Webscraping/FBRefScraper2223.py
@@ -20,10 +20,30 @@ for link in links:
 # Now we have a list of team links to next extract the player stats from. 
     
 
-
 # [Name, Age, Pos, Team, starts, mins, goals, non-pen goals, assists, XG, npXG, XA, crdY, crdR]
-
-#results = []
+scraper2 = cloudscraper.create_scraper(delay=15, browser="chrome")
+results = []
+for (team, link) in team_links:
+    html_text2 = scraper2.get(link, allow_redirects=False).text
+    player_data_soup = BeautifulSoup(html_text2, 'html.parser')
+    player_list = player_data_soup.find(id='div_stats_standard_9').find('tbody').find_all('tr')
+    for player in player_list:
+        name = player.find('td', {'data-stat': 'player'}).find('a').text
+        pos = player.find('td', {'data-stat': 'position'}).text
+        age = player.find('td', {'data-stat': 'age'}).text
+        starts = player.find('td', {'data-stat': 'games_starts'}).text
+        minutes = player.find('td', {'data-stat': 'minutes'}).text
+        goals = player.find('td', {'data-stat': 'goals'}).text
+        assists = player.find('td', {'data-stat': 'assists'}).text
+        non_pen_goals = player.find('td', {'data-stat': 'goals_pens'}).text
+        crdY = player.find('td', {'data-stat': 'cards_yellow'}).text
+        crdR = player.find('td', {'data-stat': 'cards_red'}).text
+        xG = player.find('td', {'data-stat': 'xg'}).text
+        npXG = player.find('td', {'data-stat': 'npxg'}).text
+        xA = player.find('td', {'data-stat': 'xg_assist'}).text
+        results.append([name, pos, team, age, starts, minutes, goals, non_pen_goals, assists, crdY, crdR, xG, npXG, xA])
+    
+print(results)
 
 # table1 = soup.find(id="all_stats_standard").find('div', class_='table_container is_setup')
 # print(table1)
