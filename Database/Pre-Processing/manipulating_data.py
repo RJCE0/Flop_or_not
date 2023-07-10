85111c84e3e0f243ac05e0ce55fc7f11b457cce4
javonnep <javonne.porter@ymail.com>
2023-07-10 21:22:13 +0100

    feat(damanipulating_datay): ReUsing algorithm designed in alg.txt to combine the several databases into data ready for training and testing

--- Database/Pre-Processing/manipulating_data.py
+++ Database/Pre-Processing/manipulating_data.py
@@ -1,135 +1,57 @@
-# %%
 import pandas as pd
-
-# %%
-file1 = "../CSVs/FBRefDatabase2018-19.csv"
-file2 = "../CSVs/FBRefDatabase2019-20.csv"
-file3 = "../CSVs/FBRefDatabase2020-21.csv"
-file4 = "../CSVs/FBRefDatabase2021-22.csv"
-file5 = "../CSVs/FBRefDatabase2022-23.csv"
-files = [file1, file2, file3, file4, file5]
-
-# %%
-df = pd.read_csv(file1)
-df.head(5)
-
-# %% [markdown]
-# # Add years to the dataframe
-
-# %%
-# This might break other code lol
-
-# i = 0
-# for file in files:
-#     df = pd.read_csv(file)
-#     df["Year"] = i
-#     df.to_csv(file, index=False, na_rep='Unknown')
-#     i += 1
-    
-
-# %% [markdown]
-# # The algorithm the combine the data
-
-# %%
-# 1. Find a player P
-# 2. Find their club C, and the year Y
-# 3. Get all of the stats of the Starting 11 for C in the year (Y - 1)
-#     You should now have player P's stats in the year Y alongside the starting
-#     11 for C in the year (Y - 1). Player P should be playing for C in the year
-#     Y.
-# 4. Organise that into 1 row in with a fixed length (NN has fixed input)
-# 5. From here we can do other things (PCA, Removing rows w mcl, scalling etc)
-
-# Notes:
-# - Ideally we would choose all players on a team, but we have to choose a fixed 
-# number of players because of the NNs fixed input size
-# - If a player only exists in 1 year Y and not in another year Y-1, they will 
-# have to be removed from the dataset.
-
-# %% [markdown]
-# # 0. Extra stuff
-
-# %%
-# finding all clubs
-                    # all_clubs = []
-
-                    # for file in files:
-                    #     df = pd.read_csv(file)
-                    #     all_clubs += list(df["Club Name"])
-                    #     # clubs_in_season = set(clubs_in_season)
-                    #     # all_clubs.append(clubs_in_season)
-                    #     # all_clubs.append(list(df["Club Name"]))
-
-                    # # all_clubs = set(all_clubs)
-                    # all_clubs = set(all_clubs)
-                    # all_clubs
-                    # # set(df["Club Name"])
-# restricting positions
-                    # df["Position"] = df["Position"].str[:2]
+import numpy
 
 
-# %% [markdown]
-# # 1. Find a player P 
-# # 2. Find their club C, and the year Y
-# 
-# 
+# Begin with tuples of (y, y-1) where y is the dtabase from year y 
+files_dict = {
+    "2022-23": "Database/CSVs/FBRefDatabase2022-23.csv",
+    "2021-22": "Database/CSVs/FBRefDatabase2021-22.csv",
+    "2020-21": "Database/CSVs/FBRefDatabase2020-21.csv",
+    "2019-20": "Database/CSVs/FBRefDatabase2019-20.csv",
+    "2018-19": "Database/CSVs/FBRefDatabase2018-19.csv",
+}
+# List = (Y, Y-1)
+year_pairs = [(files_dict["2022-23"], files_dict["2021-22"]), 
+     (files_dict["2021-22"], files_dict["2020-21"]),
+     (files_dict["2020-21"], files_dict["2019-20"]),
+     (files_dict["2019-20"], files_dict["2018-19"]),
+    ]
 
-# %%
-# print(df.iloc[3])
-print(df.iloc[3]["Player Name"])
-print(df.iloc[3]["Position"])
-print(df.iloc[3]["Club Name"])
-print(df.iloc[3]["Year"])
+df = pd.DataFrame([])
 
-name = df.iloc[3]["Player Name"]
-pos  = df.iloc[3]["Position"]
-club = df.iloc[3]["Club Name"]
-year = df.iloc[3]["Year"]
-
-# %% [markdown]
-# # 3. Get all of the stats of the Starting 11 for C in the year (Y - 1)
-# 
-
-# %%
-next_year = year + 1
-
-# %%
-# sorting by club and minutes
-df.sort_values(by=["Mins"])
-df.groupby("Club Name")
-
-
-# %%
-# we have next_year, and the club name 
-player_stats = list(df.loc[0][1:-1])
-player_stats
-
-# %%
-team_df = df[df["Club Name"] == 0]
-team_df
-
-# %%
-stats_as_list = []
-for i in range(min(15,len(team_df))):
-    stats_as_list += list(team_df.loc[i][1:-1])
-
-stats_as_list
-
-# %%
-# You should now have player P's stats in the year Y alongside the starting
-# 11 for C in the year (Y - 1). Player P should be playing for C in the year Y.
-
-# %%
-# 4. Organise that into 1 row in with a fixed length (NN has fixed input)
-# 5. From here we can do other things (PCA, Removing rows w mcl, scalling etc)
-
-# Notes:
-# - Ideally we would choose all players on a team, but we have to choose a fixed 
-# number of players because of the NNs fixed input size
-# - If a player only exists in 1 year Y and not in another year Y-1, they will 
-# have to be removed from the dataset.
-
-# %%
+# For every year pair that we have, we want to get the first entry in the tuple which is the 
+# file name of the database for that specific year. Then once retrieved we want to go through each entry and
+# take the player data for this year and couple it with the team data for the second entry in year pair. 
+for year_pair in year_pairs:
+    # (files_dict["2022-23"], files_dict["2021-22"])
+    df1 = pd.read_csv(year_pair[0])
+    df2 = pd.read_csv(year_pair[1])
+    for i in range(len(df1)):
+        
+        # Select the ith entry from the current year of players in the database
+        curr_year_player = df1.loc[i]
+        
+        #Find the current players team 
+        team = curr_year_player["Club Name"]
+        
+        # Find the players stats for the previous campaign if exists 
+        prev_year_player = df2[df2["Player Name"] == curr_year_player["Player Name"]]
+        
+        # If-else check for if player is in prev database if not skip player by breaking from else
+        if not prev_year_player.empty:
+            # Find the teams squad stats for the previous campaign
+            squad_stats_prev_year = df2[df2["Club Name"] == team]
+        
+            # Removing curr player from prev year squad dataset to ensure unique 15 players
+            squad_stats_prev_year = squad_stats_prev_year[squad_stats_prev_year["Player Name"] != curr_year_player["Player Name"]]
+            
+            # Get top top 15 players by minutes played
+            dfe = squad_stats_prev_year.nlargest(15, 'Mins')
+        
+            # TODO: Combine curr player stats, their prev year, and squad stats to one row
+            
+        else:
+            break
 
 
 
