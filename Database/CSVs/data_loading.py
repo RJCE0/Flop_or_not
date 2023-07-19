4136e8b6effc7b393286401d5d9ff60dea2819c4
javonnep <javonne.porter@ymail.com>
2023-07-19 20:59:49 +0100

    refactor(CSVs) Refectored CSV file structure

---
+++ Database/CSVs/data_loading.py
@@ -0,0 +1,19 @@
+# import numpy as np
+# import pandas as pd
+
+# filename = "Dataset.csv"
+# df = pd.read_csv(filename)
+# print(df.head())
+
+# x = np.array(df, dtype=int)
+# print(x)
+
+
+
+
+
+
+x = 8
+
+
+
